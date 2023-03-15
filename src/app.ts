8b4df308569e40e2ce5ed445d18f6a98569f2c06
rusikstayhome <alenrus32@gmail.com>
2023-03-15 11:50:25 +0200

    added express error handling

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,28 @@
 import 'dotenv/config';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import ExerciseModel from './models/exercise';
 
 const app = express();
 
-app.get('/', async (req, res) => {
-  const exercises = await ExerciseModel.find().exec();
-  res.status(200).json(exercises);
+app.get('/', async (req, res, next) => {
+  try {
+    const exercises = await ExerciseModel.find().exec();
+    res.status(200).json(exercises);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use((req, res, next) => {
+  next(Error('Endpoint not found'));
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.error(error);
+  let errorMessage = 'An unknown error occured';
+  if (error instanceof Error) errorMessage = error.message;
+  res.status(500).json({ error: errorMessage });
 });
 
 export default app;
